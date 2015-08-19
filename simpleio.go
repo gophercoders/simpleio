9c98be262cb652e126df7ddaef618fd878bcf7d7
Owen Waller <o.waller@kulawe.com>
2015-08-19 16:17:11 +0100

    Complete rewrite of the package.
    This is a complete rewrite of the package. The package has been
    rewritten to support:
    
    Strings
    Integers - both positive and negative
    Floats - both positive and negative
    
    The previous version did not support negative integers (or
    positive integers if a + was supplied), or floats at all.
    
    Internally the module is now based around a bufio.Scanner rather
    that a bufio.Reader. The module has also been completely refactored
    to make it easily testable. A test package is supplied.
    
    The module is not idiomatic Go and is not intended to be. Likewise
    the package is not go routine safe and is not intended to be.

--- simpleio.go
+++ simpleio.go
@@ -8,37 +8,120 @@ import (
 	"strings"
 )
 
-var reader *bufio.Reader
+var scanner *bufio.Scanner
 
 func init() {
-	reader = bufio.NewReader(os.Stdin)
+	scanner = bufio.NewScanner(os.Stdin)
 }
 
 func ReadStringFromKeyboard() string {
-	s, _ := reader.ReadString('\n')
-	return strings.TrimSpace(s)
+	s, errorStr := readStringFromKeyboard(scanner)
+	if errorStr != "" {
+		fmt.Println(errorStr)
+	}
+	return s
+}
+
+func readStringFromKeyboard(scanner *bufio.Scanner) (string, string) {
+	var s string
+	var errorStr string
+	for scanner.Scan() {
+		s = strings.TrimSpace(scanner.Text())
+		// there was no error sowe can return the string here. This
+		// limits the function to the first line only.
+		return s, errorStr
+	}
+	// The scanner stoped. Why?
+	if err := scanner.Err(); err != nil {
+		errorStr = fmt.Sprintf("Sorry I could not scan the line. Error: %v. Try again...", err)
+	}
+	// in the case of an error we return the empty string.
+	return s, errorStr
 }
 
 func ReadNumberFromKeyboard() int {
-	var s string
-	ok := false
-	for !ok {
-		s, _ = reader.ReadString('\n')
-		s = strings.TrimSpace(s)
-		for _, c := range s {
-			if c >= '0' && c <= '9' {
-				ok = true
+	i, s := readNumberFromKeyboard(scanner)
+	if s != "" {
+		fmt.Println(s)
+	}
+	return i
+}
+
+func readNumberFromKeyboard(scanner *bufio.Scanner) (int, string) {
+	var errorStr string
+	for scanner.Scan() {
+		s := strings.TrimSpace(scanner.Text())
+		i, err := strconv.ParseInt(s, 10, 0)
+		if err != nil {
+			if err, ok := err.(*strconv.NumError); ok {
+				switch err.Err {
+				case strconv.ErrSyntax:
+					errorStr = "Sorry I don't think that was a number. Try again..."
+				case strconv.ErrRange:
+					if i > 0 {
+						errorStr = "Sorry that number was too big. Try again..."
+						i = 0
+					} else {
+						errorStr = "Sorry that number was too small. Try again..."
+						i = 0
+					}
+				}
 			} else {
-				ok = false
-				break
+				panic("The error type returned by strconv.ParseInt is NOT an *strconv.NumError!. This contradics the ParseInt docs.")
 			}
 		}
-		if !ok {
-			fmt.Println("Sorry I don't think that was a number. Try again...")
-		}
+		// return the first number we find.
+		// i always has a value at this point See the docs for ParseInt
+		// http://godoc.org/strconv#ParseInt
+		return int(i), errorStr
 	}
+	// The scanner stopped. Why?
+	if err := scanner.Err(); err != nil {
+		errorStr = fmt.Sprintf("Sorry I could not scan the line. Error: %v. Try again...", err)
+	}
+	// in the case of an error we return zero
+	return 0, errorStr
+}
 
-	n, _ := strconv.Atoi(s)
-	return n
+func ReadDecimalFractionFromKeyboard() float64 {
+	i, s := readDecimalFractionFromKeyboard(scanner)
+	if s != "" {
+		fmt.Println(s)
+	}
+	return i
+}
 
+func readDecimalFractionFromKeyboard(scanner *bufio.Scanner) (float64, string) {
+	var errorStr string
+	for scanner.Scan() {
+		s := strings.TrimSpace(scanner.Text())
+		f, err := strconv.ParseFloat(s, 64)
+		if err != nil {
+			if err, ok := err.(*strconv.NumError); ok {
+				switch err.Err {
+				case strconv.ErrSyntax:
+					errorStr = "Sorry I don't think that was a number. Try again..."
+				case strconv.ErrRange:
+					if f > 0 {
+						errorStr = "Sorry that number was too big. Try again..."
+					} else {
+						errorStr = "Sorry that number was too small. Try again..."
+					}
+					f = 0.0
+				}
+			} else {
+				panic("The error type returned by strconv.ParseParse is NOT an *strconv.NumError!. This contradics the ParseFloat docs.")
+			}
+		}
+		// return the first number we find.
+		// f always has a value at this point See the docs for ParseFloat
+		// http://godoc.org/strconv#ParseFloat
+		return f, errorStr
+	}
+	// The scanner stoped. Why?
+	if err := scanner.Err(); err != nil {
+		errorStr = fmt.Sprintf("Sorry I could not scan the line. Error: %v. Try again...", err)
+	}
+	// in the case of an error we return 0.0
+	return 0.0, errorStr
 }
---
+++ simpleio_test.go
@@ -0,0 +1,139 @@
+package simpleio
+
+import (
+	"bufio"
+	"io"
+	"strings"
+	"testing"
+)
+
+type failingReader struct{}
+
+func (failingReader) Read(p []byte) (int, error) {
+	return 0, io.ErrUnexpectedEOF
+}
+
+var stringTestResults = []struct {
+	s             string
+	expectedStr   string
+	expectedError string
+}{
+	{"123", "123", ""},
+	{"-123", "-123", ""},
+	{"    abc\n", "abc", ""},
+	{"xyz    \n", "xyz", ""},
+	{"    ijk    \n", "ijk", ""},
+}
+
+func TestReadStringFromKeyboard(t *testing.T) {
+	for _, td := range stringTestResults {
+		sr := strings.NewReader(td.s)
+		scanner := bufio.NewScanner(sr)
+		readString, errorStr := readStringFromKeyboard(scanner)
+		if readString != td.expectedStr {
+			t.Fatalf("expected \"%s\" but got \"%s\"\n", td.expectedStr, readString)
+		}
+		if errorStr != td.expectedError {
+			t.Fatalf("expected \"%s\" but got \"%s\"\n", td.expectedStr, readString)
+		}
+	}
+}
+
+func TestReadStringFromKeyboardFailScanner(t *testing.T) {
+	scanner := bufio.NewScanner(failingReader{})
+	s, errorStr := readStringFromKeyboard(scanner)
+	expectedStr := ""
+	if s != expectedStr {
+		t.Fatalf("Did not get expected string. Expected \"%s\" but got \"%s\"\n", expectedStr, s)
+	}
+	expectedError := "Sorry I could not scan the line. Error: unexpected EOF. Try again..."
+	if errorStr != expectedError {
+		t.Fatalf("Did not get expected error string. Expected \"%s\" but got \"%s\"\n", expectedError, errorStr)
+	}
+}
+
+var numberTestResults = []struct {
+	s              string
+	expectedNumber int
+	expectedError  string
+}{
+	{"123", 123, ""},
+	{"-123", -123, ""},
+	{"    234\n", 234, ""},
+	{"345    \n", 345, ""},
+	{"    456    \n", 456, ""},
+	{"123x", 0, "Sorry I don't think that was a number. Try again..."},
+	{"abc", 0, "Sorry I don't think that was a number. Try again..."},
+	{"1234456778909876543211234567890909876654332123434556787890", 0, "Sorry that number was too big. Try again..."},
+	{"-1234456778909876543211234567890909876654332123434556787890", 0, "Sorry that number was too small. Try again..."},
+}
+
+func TestReadNumberFromKeyboard(t *testing.T) {
+	for _, td := range numberTestResults {
+		sr := strings.NewReader(td.s)
+		scanner := bufio.NewScanner(sr)
+		readNumber, errorStr := readNumberFromKeyboard(scanner)
+		if readNumber != td.expectedNumber {
+			t.Fatalf("Did not get expected number. Expected \"%d\" but got \"%d\"\n", td.expectedNumber, readNumber)
+		}
+		if errorStr != td.expectedError {
+			t.Fatalf("Did not get expected error string. Expected \"%s\" but got \"%s\"\n", td.expectedError, errorStr)
+		}
+	}
+}
+
+func TestReadNumberFromKeyboardFailScanner(t *testing.T) {
+	scanner := bufio.NewScanner(failingReader{})
+	readNumber, errorStr := readNumberFromKeyboard(scanner)
+	expectedNumber := 0.0
+	if readNumber != 0.0 {
+		t.Fatalf("Did not get expected number. Expected \"%f\" but got \"%f\"\n", expectedNumber, readNumber)
+	}
+	expectedError := "Sorry I could not scan the line. Error: unexpected EOF. Try again..."
+	if errorStr != expectedError {
+		t.Fatalf("Did not get expected error string. Expected \"%s\" but got \"%s\"\n", expectedError, errorStr)
+	}
+}
+
+var decimalFractionTestResults = []struct {
+	s              string
+	expectedNumber float64
+	expectedError  string
+}{
+	{"3.14", 3.14, ""},
+	{"-3.14", -3.14, ""},
+	{"    2.71828\n", 2.71828, ""},
+	{"1.4142    \n", 1.4142, ""},
+	{"    0.69314    \n", 0.69314, ""},
+	{"123.x", 0, "Sorry I don't think that was a number. Try again..."},
+	{"abc.123", 0, "Sorry I don't think that was a number. Try again..."},
+	{"123445677890987654321123456789090987665433212343455678789012344567789098765432112345678909098766543321234345567878901234456778909876543211234567890909876654332123434556787890123445677890987654321123456789090987665433212343455678789012344567789098765432112345678909098766543321234345567878901234456778909876543211234567890909876654332123434556787890.1234353", 0.0, "Sorry that number was too big. Try again..."},
+	{"-123445677890987654321123456789090987665433212343455678789012344567789098765432112345678909098766543321234345567878901234456778909876543211234567890909876654332123434556787890123445677890987654321123456789090987665433212343455678789012344567789098765432112345678909098766543321234345567878901234456778909876543211234567890909876654332123434556787890.211324325", 0.0, "Sorry that number was too small. Try again..."},
+}
+
+func TestReadDecimalFractionFromKeyboard(t *testing.T) {
+	for _, td := range decimalFractionTestResults {
+		sr := strings.NewReader(td.s)
+		scanner := bufio.NewScanner(sr)
+		readNumber, errorStr := readDecimalFractionFromKeyboard(scanner)
+		if readNumber != td.expectedNumber {
+			t.Fatalf("Did not get expected number. Expected \"%f\" but got \"%f\"\n", td.expectedNumber, readNumber)
+		}
+		if errorStr != td.expectedError {
+			t.Fatalf("Did not get expected error string. Expected \"%s\" but got \"%s\"\n", td.expectedError, errorStr)
+		}
+	}
+}
+
+func TestReadDecimalFractionFromKeyboardFailScanner(t *testing.T) {
+	scanner := bufio.NewScanner(failingReader{})
+	readNumber, errorStr := readDecimalFractionFromKeyboard(scanner)
+	expectedNumber := 0.0
+	if readNumber != 0.0 {
+		t.Fatalf("Did not get expected number. Expected \"%f\" but got \"%f\"\n", expectedNumber, readNumber)
+	}
+	expectedError := "Sorry I could not scan the line. Error: unexpected EOF. Try again..."
+	if errorStr != expectedError {
+		t.Fatalf("Did not get expected error string. Expected \"%s\" but got \"%s\"\n", expectedError, errorStr)
+	}
+}
